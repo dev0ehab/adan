c56ee4c6d5304029b6bd81f5da7f324492530140
Ehab Barakat <dev.ehab.barakat@gmail.com>
2026-04-04 22:24:09 +0200

    Update cors.php

--- config/cors.php
+++ config/cors.php
@@ -1,17 +1,12 @@
 <?php
 
 return [
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => [
-        'http://localhost:5173',
-        'http://localhost:3000',
-        'http://localhost:8080',
-        env('FRONTEND_URL', 'http://localhost:5173'),
-    ],
+    'allowed_origins' => ['*'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
-    'supports_credentials' => true,
+    'supports_credentials' => false,
 ];
